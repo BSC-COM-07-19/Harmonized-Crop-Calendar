7a158b47191d67901883765792f31c2df7ee8a94
BSC-COM-03-19-CHIKALUSA <bsc-com-03-19@unima.ac.mw>
2024-05-08 20:41:49 +0200

    Weather Forecasting Code Using OpenWeatherMap API and RapidAPI

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Home from './pages/Home';
 import Graph from './pages/Graph';
+import WeatherApp from './components/WeatherApp';
 
 function App() {
   return (
     <div className="App">
       <Home />
       <Graph />
+      <WeatherApp/>
     </div>
   );
 }
