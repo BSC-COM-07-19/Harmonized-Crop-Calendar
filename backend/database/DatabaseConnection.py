8d839ffd3989dbc47ca995e589fe65e080d7e315
EzekielBanda <bsc-com-10-19@unima.ac.mw>
2024-06-20 03:21:04 +0200

    changeg url for deployment

--- backend/database/DatabaseConnection.py
+++ backend/database/DatabaseConnection.py
@@ -2,8 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_DATABASE = 'postgresql://harmonuser:Eh5scm7kHGcVv6XQ6zHEORxi923sfHvQ@dpg-cpn948tds78s73arqo4g-a.oregon-postgres.render.com/harmon_7jl5'
+# URL_DATABASE = 'postgresql://harmonuser:Eh5scm7kHGcVv6XQ6zHEORxi923sfHvQ@dpg-cpn948tds78s73arqo4g-a.oregon-postgres.render.com/harmon_7jl5'
 # URL_DATABASE = "postgresql://postgres:12345@localhost/harmon"
+URL_DATABASE = "postgresql://harmonuser:iV0VTSWDDq3KA988Ty7QB7OmJfSER1wZ@dpg-cppo51g8fa8c739jhrvg-a.oregon-postgres.render.com/harmon_ffkr"
 
 engine = create_engine(URL_DATABASE)
 
