72f7116efff8fda54a525627d49add04e7db0e74
Ezekiel Banda <bsc-com-10-19@unima.ac.mw>
2024-06-20 19:31:51 +0200

    merged

--- backend/database/DatabaseConnection.py
+++ backend/database/DatabaseConnection.py
@@ -2,11 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-<<<<<<< HEAD
-# URL_DATABASE = 'postgresql://harmonuser:Eh5scm7kHGcVv6XQ6zHEORxi923sfHvQ@dpg-cpn948tds78s73arqo4g-a.oregon-postgres.render.com/harmon_7jl5'
-# URL_DATABASE = "postgresql://postgres:12345@localhost/harmon"
-=======
->>>>>>> 7d4e433a109c37027bffe50403aa139c117ce357
 URL_DATABASE = "postgresql://harmonuser:iV0VTSWDDq3KA988Ty7QB7OmJfSER1wZ@dpg-cppo51g8fa8c739jhrvg-a.oregon-postgres.render.com/harmon_ffkr"
 
 engine = create_engine(URL_DATABASE)
--- harmonised-frontend/src/App.js
+++ harmonised-frontend/src/App.js
@@ -3,10 +3,6 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Home from './pages/Home';
 import WeatherApp from './components/WeatherApp';
-<<<<<<< HEAD
-import PestDiseaseMgt from './components/PestDiseaseMgt';
-import CalendarPage from "./components/CalendarPage"; // Import the new calendar page component
-=======
 //import PestDiseaseMgt from './components/PestDiseaseMgt';
 // import SeasonalChart from './components/SeasonalChart';
 // import GroundnutsPestDisease from './components/Groundnuts';
@@ -16,7 +12,6 @@ import CalendarPage from "./components/CalendarPage"; // Import the new calendar
 // import MarketPricing from './components/MarketPricing';
 import CalendarPage from "./components/CalendarPage"; 
 import PestDiseasePage from "./components/PestDiseaseMgt";
->>>>>>> 7d4e433a109c37027bffe50403aa139c117ce357
 
 const App = () => {
   return (
@@ -27,10 +22,6 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/pests-diseases/:crop" element={<PestDiseasePage  />} /> 
           <Route path="/weather" element={<WeatherApp />} />
-<<<<<<< HEAD
-          <Route path="/pests" element={<PestDiseaseMgt />} />
-=======
->>>>>>> 7d4e433a109c37027bffe50403aa139c117ce357
           <Route path="/calendar" element={<CalendarPage />} />
         </Routes>
       </div>
